ebc915165bf81458b7c40e3a671024b2009fc725
Jacob Wood <jacobwood06132009@gmail.com>
2026-06-17 09:51:39 +0100

    student management version 1.3

--- student_management.py
+++ student_management.py
@@ -1,7 +1,7 @@
 #Student management system 
 import sqlite3
 
-def Search_Feature():
+def Search_Feature(cursor):
         #this connects to the database
         conn = sqlite3.connect("Student_Records.db")
         cursor = conn.cursor()
@@ -9,14 +9,14 @@ def Search_Feature():
         #gets the search term from the user
         term = input("\nEnter Student Name or ID to search:").strip()
 
-        query = "SELECT * FROM students WHERE id = ? OR name LIKE ?"
+        query = "SELECT * FROM students WHERE ID = ? OR name LIKE ? OR last_name Like ?"
         search_pattern = f"%{term}%"
 
         #We pass the search terms as a tuple to match the two '?' placeholders
-        cursor.execute(query, (term, search_pattern))
+        cursor.execute(query, (term, search_pattern, search_pattern))
 
         results = cursor.fetchall()
-        #helps with error handling
+        #helps with error handling2
         if not results:
             print(f"\n No student records found matching '{term}'.")
         else:
@@ -25,18 +25,17 @@ def Search_Feature():
 
             for row in results: 
             #this unpacks the tuple data type into clear variables
-                student_id, name, course, grades = row
-                print(f"ID:{student_id} | Name {first_name} {last_name} | Course: {course} | Grade: {grade}")
-        #Closing the connection when done reading
-        conn.close()
+                f_name, l_name, s_id, crs, grd = row
+                print(f"ID: {s_id} | Name: {f_name} {l_name} | Course: {crs} | Grade: {grd}")
 
-def Delete_Feature():
+#Conn and cursor parameters so changes save permanently
+def Delete_Feature(conn, cursor):
     #the conn is there to connect the database
     conn = sqlite3.connect("Student_Records.db")
     cursor = conn.cursor()
     target_ID = input("\nEnter Student name or ID to delete:").strip()
 
-    check_query = "DELETE FROM students WHERE id = ?"
+    check_query = "DELETE FROM students WHERE ID = ?"
     cursor.execute(check_query, (target_ID))
     #we only need to provide one match
     student = cursor.fetchone() 
@@ -47,11 +46,11 @@ def Delete_Feature():
         print(f"\n Error: No student found with ID {target_ID}.")
     else:
         #this will unoack the found record to show the user who they are deleting
-        student_id, name, course, grade = student 
-        print(f"\n Record Found: {first_name} {last_name}| (Course: {course})")
+        s_id, f_name, l_name, crs, grd = student 
+        print(f"\n Record Found: {f_name} {l_name}| (Course: {crs})")
 
         #The confirmation will double check with the user before wiping data
-        confirm = input(f"Are you sure you want to permanently delete the {first_name} {last_name}? (yes/no):").strip().lower()
+        confirm = input(f"Are you sure you want to permanently delete the {f_name} {l_name}? (yes/no):").strip().lower()
         if confirm == "yes":
 
             #the 'DELETE' is to run the deletion query
@@ -59,25 +58,48 @@ def Delete_Feature():
 
             cursor.execute(delete_query, (target_ID,))
 
-            #The commit is there to premanently save the changes to the datavase file
+            #The commit is there to permanently save the changes to the database file
             conn.commit()
 
-            print(f"\n Sucess: Student record for '{first_name}{last_name}' has been deleted.")
+            print(f"\n Sucess: Student record for '{f_name}{l_name}' has been deleted.")
         else: 
             print("\n Deletion cancelled. Record was not changed.")
 
-    # always close the connection so no errors are involved
-    conn.close()
-
-
-    
 # Creates the database to store student records and also creates cursor to execute SQL commands
 def create_database():
-    connect = sqlite3.connect("Student_Records.db")  # Creates/connects to database file
-    cursor = connect.cursor()  # Allows execution of SQL commands
-    return connect, cursor
+    conn = sqlite3.connect("Student_Records.db")  # Creates/connects to database file
+    cursor = conn.cursor()  # Allows execution of SQL commands
+    return conn, cursor
 
-# Creates the table of student records
+def Update_feature(conn, cursor):
+    conn = sqlite3.connect("Student_Records.db") #connects the database
+    cursor = conn.cursor()
+    target_ID = input("\nEnter Student ID  to update:").strip()
+    check_query = "SELECT * FROM students WHERE ID = ?"
+    cursor.execute(check_query, (target_ID,))
+    student = cursor.fetchone()
+
+    if not student:
+        print(f"\n Error: No student found with ID {target_ID}.")
+    else:
+        f_name, l_name, crs, grd = student
+        print(f"\n CUrrent Record: {f_name} {l_name} | Course: {crs} | Grade: {grd}")
+        print(f"Leabe blank and press Enter to keep the current information")
+
+        #If the user presses enter without typing, using the or to operator to keep old data make it easy
+        new_first = input(f"Enter new first name ({f_name}): ").strip() or f_name
+        new_last = input(f"Enter new first name ({l_name}): ").strip() or l_name
+        new_course = input(f"Enter new first name ({crs}): ").strip() or crs
+        new_grade = input(f"Enter new first name ({grd}): ").strip() or grd
+
+        #? = to the placeholders to store information
+        update_query = """
+        UPDATE students
+        SET first_name = ?, last_name = ?, course = ?, graden = ?
+        WHERE ID = ?
+        """
+        cursor.execute(update_query, (new_first, new_last, new_course, new_grade, target_ID))
+
 def create_table(cursor):
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS students (   -- prevents error if table already exists
@@ -132,12 +154,16 @@ def mainProgram():
             course = input("Enter course: ")
             grade = input("Enter grade: ")
 
+            insert_studentrecords(cursor, first_name, last_name, course, grade)
+            connect.commit()
+            print("Student added successfully")
+
         elif choice == "2": 
             #calls for all student records here
             view_students(cursor)
 
         elif choice == "3":
-            Search_Feature()
+            Search_Feature(cursor)
             #calls the student search function
             
         elif choice == "4":
@@ -145,7 +171,7 @@ def mainProgram():
             print("Opening student's information...")
         
         elif choice == "5":
-            Delete_Feature()
+            Delete_Feature(connect, cursor)
             #calls the delete function here
         
         elif choice == "6":
@@ -155,25 +181,8 @@ def mainProgram():
         else: 
             print("Choice is invalid please retry. :(")
             #it will validate code and make sure the user puts the correct code
-
-    
-    # Step 3: get user input
-    first_name = input("Enter first name: ")
-    last_name = input("Enter last name: ")
-    course = input("Enter course: ")
-    grade = input("Enter grade: ")
-    
-    # Step 4: insert data
-    insert_studentrecords(cursor, first_name, last_name, course, grade)
-    
-    # Step 5: save changes
-    connect.commit()
-    
-    # Step 6: display records
-    view_students(cursor)
-    
-    # Step 7: close connection
     connect.close()
+   
 if __name__ == "__main__":
     mainProgram()
 #it will call the whole main program function
